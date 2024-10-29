f2598d0cb190df06b42eae51af068358f02a0c64
Tofazzal26 <afranislamabir6789@gmail.com>
2024-10-29 22:07:39 +0600

    footer style finish

--- src/Pages/Root/Root.jsx
+++ src/Pages/Root/Root.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../../Components/Navbar/Navbar";
+import Footer from "../../Components/Footer/Footer";
 
 const Root = () => {
   return (
     <div>
       <Navbar />
-
       <Outlet />
+      <Footer />
     </div>
   );
 };
